97cd085494dd689b306ba1490b73ed7d3024ad78
hyonwoo2026 <harrycho13@naver.com>
2026-07-09 23:37:25 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -3,18 +3,14 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(200)
     basic.clearScreen()
 })
-let 깨끗한_빗물_측정 = 0
 let 초기_우수량 = 0
 OLED.init(128, 64)
+let 깨끗한_빗물_측정 = 0
 초기_우수량 = 0
 let 오류저장 = 0
 basic.pause(100)
 servos.P1.setAngle(0)
-servos.P2.setAngle(0)
-basic.forever(function () {
-    초기_우수량 = Environment.ReadWaterLevel(AnalogPin.P4)
-    깨끗한_빗물_측정 = Environment.ReadWaterLevel(AnalogPin.P5)
-})
+servos.P2.setAngle(180)
 basic.forever(function () {
     OLED.writeStringNewLine("Starting to collect dirty rainwater")
     basic.pause(200)
@@ -22,15 +18,19 @@ basic.forever(function () {
         OLED.writeStringNewLine("Trash dirty rainwater which it collected")
         basic.pause(1000)
         servos.P1.setAngle(135)
-        servos.P2.setAngle(135)
+        servos.P2.setAngle(45)
         basic.pause(5000)
         OLED.writeStringNewLine("completed removing dirty rainwater. Collecting new rainwater...")
         basic.pause(5000)
         servos.P1.setAngle(0)
-        servos.P2.setAngle(0)
+        servos.P2.setAngle(180)
         오류저장 = 1
         basic.pause(5000)
     }
+})
+basic.forever(function () {
+    초기_우수량 = Environment.ReadWaterLevel(AnalogPin.P4)
+    깨끗한_빗물_측정 = Environment.ReadWaterLevel(AnalogPin.P5)
 })
 basic.forever(function () {
     if (오류저장 == 1 && 깨끗한_빗물_측정 >= 90) {
