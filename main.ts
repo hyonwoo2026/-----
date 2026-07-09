bd7a17a1f3b17fc9a5b981db597baff6ee1d1840
hyonwoo2026 <harrycho13@naver.com>
2026-07-09 23:22:54 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
 })
 let 초기_우수량 = 0
-OLED12864_I2C.init(60)
+OLED.init(128, 64)
 초기_우수량 = 0
 let 오류저장 = 0
 basic.pause(100)
@@ -15,30 +15,15 @@ basic.forever(function () {
 })
 basic.forever(function () {
     if (0 == 0) {
-        OLED12864_I2C.showString(
-        0,
-        0,
-        "Starting to collect dirty rainwater",
-        1
-        )
+        OLED.writeStringNewLine("Starting to collect dirty rainwater")
         basic.pause(200)
         if (오류저장 == 0 && 초기_우수량 >= 60) {
-            OLED12864_I2C.showString(
-            0,
-            0,
-            "Trash dirty rainwater which it collected",
-            1
-            )
+            OLED.writeStringNewLine("Trash dirty rainwater which it collected")
             basic.pause(1000)
             servos.P1.setAngle(135)
             servos.P2.setAngle(135)
             basic.pause(5000)
-            OLED12864_I2C.showString(
-            0,
-            0,
-            "completed removing dirty rainwater. Collecting new rainwater...",
-            1
-            )
+            OLED.writeStringNewLine("completed removing dirty rainwater. Collecting new rainwater...")
             basic.pause(5000)
             servos.P1.setAngle(0)
             servos.P2.setAngle(0)
@@ -49,12 +34,6 @@ basic.forever(function () {
 })
 basic.forever(function () {
     if (오류저장 == 1 && 초기_우수량 >= 90) {
-        OLED12864_I2C.showString(
-        0,
-        0,
-        "Completed removing clean rainwater",
-        1
-        )
-        basic.showString("Completed removing clean rainwater")
+        OLED.writeStringNewLine("Completed removing clean rainwater")
     }
 })
